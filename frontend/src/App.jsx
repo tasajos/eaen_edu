ad48a46e1a16c0f33e6d7a9d8324e297f344a892
Carlos Azcarraga <tasajos@gmail.com>
2026-02-15 16:51:14 -0400

    update_cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginEAEN from "./components/LoginEAEN/LoginEAEN";
+import DashboardJefe from "./components/Dashboard/Jefeunidad/Dashboardjefe";
 
 export default function App() {
   return (
-    <LoginEAEN
-      onLoginSuccess={(session) => {
-        console.log("Login DEMO OK:", session);
-        // Más adelante aquí haremos el navigate al dashboard
-      }}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginEAEN />} />
+        <Route path="/dashboard-jefe" element={<DashboardJefe />} />
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
